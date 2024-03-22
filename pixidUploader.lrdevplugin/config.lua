d07f9430474e9a749a703e11957964d373ad1ec7
Chaochana Siparitat <chaochana@gmail.com>
2024-03-22 15:06:45 +0700

    remove interval default setting

--- pixidUploader.lrdevplugin/config.lua
+++ pixidUploader.lrdevplugin/config.lua
@@ -2,11 +2,10 @@ config = {}
 
 -- config['lightroomFolder'] = ''
 -- config['size'] = ''
--- config['interval'] = ''
 -- config['outputFolder'] = ''
 -- config['ftpIsEnabled'] = false
 -- config['ftpUsername'] = ''
 -- config['ftpPassword'] = ''
 -- config['presetsInFavoriteIsApplied'] = false
 
-config['sleep'] = 10
+config['sleep'] = 5
